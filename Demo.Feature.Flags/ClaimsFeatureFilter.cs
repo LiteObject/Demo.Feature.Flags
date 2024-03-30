44cffc02de23b84179ef606dd223f73d2c9b05cb
LiteObject <Lite.Object@gmail.com>
2024-03-30 09:59:29 -0500

    Update packages, upgrade framework

--- Demo.Feature.Flags/ClaimsFeatureFilter.cs
+++ Demo.Feature.Flags/ClaimsFeatureFilter.cs
@@ -18,13 +18,18 @@ namespace Demo.Feature.Flags
             var settings = context.Parameters.Get<ClaimsFilterSettings>();
 
             // Retrieve the current user (ClaimsPrincipal)
-            var user = _httpContextAccessor.HttpContext.User;
+            System.Security.Claims.ClaimsPrincipal? user = _httpContextAccessor.HttpContext?.User;
 
             // Only enable the feature if the user has ALL the required claims
-            var isEnabled = settings.RequiredClaims
-                .All(claimType => user.HasClaim(claim => claim.Type == claimType));
+            bool isEnabled = settings.RequiredClaims
+                .All(claimType => user?.HasClaim(claim => claim.Type == claimType));
 
             return isEnabled;
+        }
+
+        public Task<bool> EvaluateAsync(FeatureFilterEvaluationContext context)
+        {
+            throw new NotImplementedException();
         }
     }
 }
